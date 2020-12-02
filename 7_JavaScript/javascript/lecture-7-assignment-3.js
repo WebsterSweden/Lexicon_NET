996c68c0ccd68388136bf5d16accdba2d7cc9d23
WebsterSweden <webster@webster.se>
2020-12-02 19:58:31 +0100

    Edit 7_JavaScript.

--- 7_JavaScript/javascript/lecture-7-assignment-3.js
+++ 7_JavaScript/javascript/lecture-7-assignment-3.js
@@ -4,11 +4,3 @@ browser, devel, es6
 /*global
 window
 */
-
-function dateNowFunction() {
-    'use strict';
-    var dateNow = new Date();
-    document.getElementById("dateNow").innerText = dateNow;
-}
-
-document.getElementById("dateNowButton").addEventListener('click', dateNowFunction);
