6d9124229ab792322e87c60f9e62461aaec529d7
WebsterSweden <webster@webster.se>
2020-12-15 00:14:42 +0100

    Edit 7_JavaScript.

--- 7_JavaScript/javascript/lecture-7-assignment-1.js
+++ 7_JavaScript/javascript/lecture-7-assignment-1.js
@@ -8,3 +8,32 @@ window
 // bitwise, browser, convert, couch, devel, es6, eval, fudge, getset, long, node, single, this, white
 
 console.log(document.currentScript);
+
+let stringOne = document.getElementById("stringOneInput").innerHTML;
+
+let index;
+let stringOneSplit;
+let stringOneInterval;
+stringOneSplit = stringOne.split("");
+let arrayOnePop = [];
+let arrayOneUnshift = [];
+let arrayOneConcat = [];
+
+function stringOneMove() {
+    'use strict';
+    arrayOnePop = stringOneSplit.pop();
+    arrayOneUnshift.unshift(arrayOnePop);
+    arrayOneConcat = arrayOneConcat.concat(arrayOneUnshift);
+    document.getElementById("stringOneOutput").innerHTML += arrayOneUnshift.join("") + stringOneSplit.join("") + "<br>";
+    if (stringOneSplit.length === 0) {
+        clearInterval(stringOneInterval);
+    }
+}
+
+function stringOneTimerStart() {
+    'use strict';
+    document.getElementById("stringOneOutput").innerHTML += stringOneSplit.join("") + "<br>";
+    stringOneInterval = setInterval(stringOneMove, 1000);
+}
+
+stringOneTimerStart();
